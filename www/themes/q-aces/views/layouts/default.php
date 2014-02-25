3bd5ce0230962cd507ba18b7c2f9878ba680cf24
ulasevichv <ulasevichv@teqniksoft.com>
2014-02-25 14:19:22 +0300

    Added bootstrap assets

--- www/themes/q-aces/views/layouts/default.php
+++ www/themes/q-aces/views/layouts/default.php
@@ -1,6 +1,18 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html lang="en">
 	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+		
+		<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl.'/assets/css/bootstrap.min.css'; ?>" />
+		<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl.'/assets/css/bootstrap-theme.min.css'; ?>" />
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="<?php echo Yii::app()->theme->baseUrl.'/assets/js/bootstrap.min.js'; ?>"></script>
+		
 	</head>
 	<body>
 		<?php echo $content; ?>
