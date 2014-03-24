9924d630ebafd0af54c2a4b69a5ba9efb98fcf7b
ulasevichv <ulasevichv@teqniksoft.com>
2014-03-24 18:22:52 +0300

    Marketing demo - animated map

--- www/protected/views/site/demoMarketing.php
+++ www/protected/views/site/demoMarketing.php
@@ -11,6 +11,7 @@ Yii::app()->clientScript->registerCssFile('http://fonts.googleapis.com/css?famil
 		<li data-target="#mainCarousel" data-slide-to="0" class="active"></li>
 		<li data-target="#mainCarousel" data-slide-to="1"></li>
 		<li data-target="#mainCarousel" data-slide-to="2"></li>
+		<li data-target="#mainCarousel" data-slide-to="3"></li>
 	</ol>
 	
 	<div class="carousel-inner">
@@ -67,6 +68,38 @@ Yii::app()->clientScript->registerCssFile('http://fonts.googleapis.com/css?famil
 			<div class="carousel-caption">
 			</div>
 		</div>
+		<div class="item">
+			<img />
+			<div class="_content">
+				<div class="_textBlack _textPart1">
+					Only one law firm from each law practice area (Personal Injury, DUI, Divorce, etc.) and metro area will be allowed in to the FREE private beta.
+					Time is running out - sign up!
+				</div>
+				<div class="_map_container">
+					<div id="statesMap" class="_map">
+					</div>
+					<div class="_map_key_container">
+						<div class="_map_key">
+							<div class="_item">
+								<div class="_img map_circle map_circle_yellow"></div>
+								<div class="_text">1 practice/area</div>
+							</div>
+							<div class="_item">
+								<div class="_img map_circle map_circle_orange"></div>
+								<div class="_text">2 practices/area</div>
+							</div>
+							<div class="_item">
+								<div class="_img map_circle map_circle_red"></div>
+								<div class="_text">3 practices/area</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="_textBlack _textPart2">
+					Interested to see if you qualify? Want more information?<br/>Don't wait until it is too late!
+				</div>
+			</div>
+		</div>
 	</div>
 	
 	<a class="left carousel-control" href="#mainCarousel" data-slide="prev">
@@ -82,11 +115,128 @@ Yii::app()->clientScript->registerCssFile('http://fonts.googleapis.com/css?famil
 <?php
 Yii::app()->clientScript->registerScript(uniqid(), "
 	
+	var mapCoords = [
+		{x:128, y:291, cityName:'Phoenix'},
+		{x:446, y:274, cityName:'Atlanta'},
+		{x:564, y:150, cityName:'New York'},
+		{x:50,  y:263, cityName:'Los Angeles'},
+		{x:410, y:164, cityName:'Chicago'},
+		{x:317, y:312, cityName:'Dallas'},
+		{x:331, y:348, cityName:'Houston'},
+		{x:558, y:161, cityName:'Philadelphia'},
+		{x:538, y:178, cityName:'Washington D.C.'},
+		{x:536, y:380, cityName:'Ft. Lauderdale'},
+		{x:586, y:111, cityName:'Boston'},
+		{x:16,  y:205, cityName:'San Francisco'},
+		{x:60,  y:263, cityName:'Riverside'},
+		{x:462, y:146, cityName:'Detroit'},
+		{x:332, y:124, cityName:'Minneapolis'},
+		{x:387, y:271, cityName:'St. Louis'},
+		{x:217, y:202, cityName:'Denver'}
+	];
+	
+	var mapCircles = [];
+	var mapAnimationIntervalId = -1;
+	var numMapCirclesDrawn = 0;
+	
 	$(document).ready(function()
 	{
 		$('.carousel').carousel({
 		  interval: false
 		});
+		
+		resetMapCircles();
+		startMapAnimation();
 	});
+	
+	function getMapCircleByMapCoord(mapCoord)
+	{
+		for (var i = 0; i < mapCircles.length; i++)
+		{
+			var circle = mapCircles[i];
+			
+			if (circle.x == mapCoord.x && circle.y == mapCoord.y) return circle;
+		}
+		
+		return null;
+	}
+	
+	function resetMapCircles()
+	{
+		mapCircles = [];
+		
+		mapCoords.forEach(function(mapCoord, i)
+		{
+			mapCircles.push({
+				id : i,
+				x : mapCoord.x,
+				y : mapCoord.y,
+				level : 0
+			});
+		});
+	}
+	
+	function startMapAnimation()
+	{
+		mapAnimationIntervalId = window.setInterval(function () { drawMapCircle(); }, 1000);
+	}
+	
+	function drawMapCircle()
+	{
+		var jMap = $('#statesMap');
+		
+		var notFinishedCircles = [];
+		
+		mapCircles.forEach(function(circle, i)
+		{
+			if (circle.level < 3) notFinishedCircles.push(circle);
+		});
+		
+		if (notFinishedCircles.length == 0)
+		{
+			resetMapCircles();
+			jMap.html('');
+			return;
+		}
+		
+		var index = Math.floor(Math.random()*notFinishedCircles.length);
+		
+		var circle = notFinishedCircles[index];
+		
+		var newCircleSize = { x : 0, y : 0 };
+		var newCircleCssClass = '';
+		
+		switch (circle.level)
+		{
+			case 0:
+			{
+				newCircleSize = { x : 25, y : 25 };
+				newCircleCssClass = 'map_circle_yellow';
+				break;
+			}
+			case 1:
+			{
+				newCircleSize = { x : 34, y : 34 };
+				newCircleCssClass = 'map_circle_orange';
+				break;
+			}
+			case 2:
+			{
+				newCircleSize = { x : 43, y : 43 };
+				newCircleCssClass = 'map_circle_red';
+				break;
+			}
+		}
+		
+		var circleX = Math.floor(circle.x - newCircleSize.x / 2);
+		var circleY = Math.floor(circle.y - newCircleSize.y / 2);
+		
+		var style = 'position:absolute; left:'+circleX+'px; top:'+circleY+'px;';
+		var circleHtml = '<div class=\"map_circle '+newCircleCssClass+'\" style=\"'+style+'\"></div>';
+		
+		jMap.append(circleHtml);
+		
+		circle.level++;
+	}
 	
 ", CClientScript::POS_HEAD);
